6ec6820a18ddab12cdf24785882b09c1dc84fde3
MOULI-ZANGROUPS <mouli.zangroups@gmail.com>
2024-11-18 09:14:24 +0530

    Update watchlist.js

--- frontend/src/components/watchlist.js
+++ frontend/src/components/watchlist.js
@@ -14,7 +14,7 @@ export default function Watchlist() {
     const fetchWatchlist = async () => {
       if (!email) return;
       try {
-        const response = await axios.get('http://54.206.7.73:5000/api/watchlist', {
+        const response = await axios.get('http:// 13.54.253.39:5000/api/watchlist', {
           params: { userId: email }
         });
         setWatchlist(response.data);
@@ -32,7 +32,7 @@ export default function Watchlist() {
     }
     
     try {
-      await axios.delete(`http://54.206.7.73:5000/api/watchlist/${symbol}`, {
+      await axios.delete(`http:// 13.54.253.39:5000/api/watchlist/${symbol}`, {
         params: { userId: email }
       });
       
